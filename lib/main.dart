74ba4977d9ed38f34597f3a579136cd68b3c8f38
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2026-08-14 21:13:21 -0500

    proimra parte de la interfaz

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 import 'package:quizappg16/pages/home_page.dart';
 
 void main() {
-  MaterialApp(home: HomePage());
+  runApp(MaterialApp(home: HomePage(), debugShowCheckedModeBanner: false));
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,6 +5,59 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Color(0xff2F3243),
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Color(0xff2F3243),
+        title: Text("Quizz App", style: TextStyle(color: Colors.white)),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+              flex: 5,
+              child: Center(
+                child: Text(
+                  "¿Has almorzado hoy?",
+                  style: TextStyle(color: Colors.white, fontSize: 25),
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 16,
+                  vertical: 8,
+                ),
+                child: MaterialButton(
+                  color: Colors.greenAccent,
+                  minWidth: double.infinity,
+                  child: Text("Verdadero"),
+                  onPressed: () {},
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 16,
+                  vertical: 8,
+                ),
+                child: MaterialButton(
+                  color: Colors.redAccent,
+                  minWidth: double.infinity,
+                  child: Text("Falso"),
+                  onPressed: () {},
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
